e2cb495807917a00a0cd0c0a023c2bad2e58453f
Korey <koreylo9@gmail.com>
2021-10-24 20:07:22 -0700

    test database working

--- src/main/scala/Project0/Inventory.scala
+++ src/main/scala/Project0/Inventory.scala
@@ -1,5 +1,44 @@
 package Project0
 
-class Inventory {
+import java.sql.{Connection, DriverManager}
 
+
+object Inventory {
+
+  def main(args: Array[String]) : Unit = {
+    val connect = databaseConnect()
+    if(connect == 1){
+      println("Successful Connect")
+    }
+    else{
+      println("Unsuccessful")
+    }
+  }
+
+  def databaseConnect() : Int = {
+    // connect to the database named "mysql" on port 8889 of localhost
+    val url = "jdbc:mysql://localhost:3306/demodatabase"
+    val driver = "com.mysql.cj.jdbc.Driver"
+    val username = "root"
+    val password = "Korgalnol9!"
+    var connection:Connection = null
+    try {
+      Class.forName(driver)
+      connection = DriverManager.getConnection(url, username, password)
+      val statement = connection.createStatement
+      val rs = statement.executeQuery("SELECT * FROM people")
+      while (rs.next) {
+        val id = rs.getString("id")
+        val firstname = rs.getString("firstname")
+        println("host = %s, user = %s".format(id,firstname))
+      }
+
+      return 1;
+    } catch {
+      case e: Exception => e.printStackTrace
+        return 0;
+    }
+    //    connection.close
+  }
 }
+
